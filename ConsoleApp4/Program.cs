23f7542e469c229dc2bb365ab79112a0bb973f28
Cesar Prado Ribeiro Filho <cesarprado682@hotmail.com>
2019-04-25 15:59:03 -0300

    ok

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace EncryptingMessage
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			string input;
+
+			Console.WriteLine("Digite algo a ser criptografado:");
+			input = Console.ReadLine();
+						
+			Console.WriteLine(input + " " + Encryption(input));
+			Console.ReadKey();
+		}
+
+
+		public static string Encryption(string x)
+		{
+			List<char> Letras = new List<char> {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l', 'm',
+												'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+												'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+												'Q','R','S','T','U','V','W','X','Y','Z'};
+			
+			char[] message = x.ToCharArray();
+			string encrypted = "";
+			
+			foreach (char letra in message)
+			{
+				if (letra == ' ')
+				{
+					encrypted += " ";
+				}
+				else
+				{
+					int i = Letras.BinarySearch(letra);
+					char k = Letras[i + 3];
+					encrypted += k;
+				}
+			}
+			return encrypted;
 		}
 	}
 }
